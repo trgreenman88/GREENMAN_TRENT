b96fb71f3fd2a3ccbe13d060dbb0aea5b3f53065
trgreenman88 <GreenmanT1728@sduhsd.lan>
2016-09-08 09:48:40 -0700

    PYLesson_02

---
+++ PYLesson_02/Lab_02.py
@@ -0,0 +1,22 @@
+l = 8
+w = 5
+A = l * w
+
+print("The area of your rectangle is", A)
+
+name0 = "1234 Fake St,"
+name1 = " San Diego,"
+name2 = " CA,"
+name3 = " 92030"
+name4 = "Trent"
+
+print("Hi, my name is", name4 ,"and I live at", name0 + name1 + name2 + name3)
+
+b1 = 5
+b2 = 6
+b3 = 4
+SA = 2 * b1 * b3 + 2 * b1 * b2 + 2 * b3 * b2
+
+print("The surface area of your rectangle is", SA)
+
+
