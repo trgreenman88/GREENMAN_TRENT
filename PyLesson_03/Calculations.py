8bf6ffee8b6ec7a5f91c94b78882505b00ed6b94
trgreenman88 <trgreenman88@gmail.com>
2016-09-13 09:54:06 -0700

    Ex_03 Finished

---
+++ PyLesson_03/Calculations.py
@@ -0,0 +1,7 @@
+print("This program will help you to find the volume of a sphere")
+
+rad = int(input("What is the radius of your sphere?"))
+vol = ((4/3) * 3.14159265358979323 * rad * rad * rad)
+print("The volume of your sphere is...")
+print(vol)
+          
