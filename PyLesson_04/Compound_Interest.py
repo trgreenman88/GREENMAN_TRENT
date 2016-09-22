13e6daee3c252aa59aaa465e5a820290560754bf
trgreenman88 <trgreenman88@gmail.com>
2016-09-22 10:13:02 -0700

    Lesson 4

--- PyLesson_04/Compound_Interest.py
+++ PyLesson_04/Compound_Interest.py
@@ -1,8 +1,7 @@
 def amount(rpnt):
     return(p * (1 + (r / n))**(n * t))
-
 r = float(input("Give your interest rate in decimal form:"))
 p = float(input("Give your principle cost of the loan:"))
 n = float(input("Give the number of times the loan is compounded per year:"))
 t = float(input("Give the life of the loan in years:"))
-print("The total cost of your loan is" , amount(r), "dollars")
+print("The total cost of your loan is ${:<10.2f}".format (amount(r)), "")
