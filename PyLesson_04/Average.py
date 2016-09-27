0f598da12ed93e4f7645a299f81bb1a0425ac64a
trgreenman88 <trgreenman88@gmail.com>
2016-09-27 10:25:12 -0700

    Lesson 4

---
+++ PyLesson_04/Average.py
@@ -0,0 +1,4 @@
+print("This program will help you to find the average of 3 numbers")
+num1 = float(input("What is your first number?"))
+num2 = float(input("What is your second number?"))
+num3 = float(input("What is your third number?"))
---
+++ PyLesson_04/Rectangle.py
@@ -0,0 +1,7 @@
+l = float(input("What is the length of your rectangle?"))
+w = float(input("What is the width of your rectangle?"))
+def calcPerim():
+    return l * 2 + w * 2
+    
+
+print("Your rectangle is" , "{:00.5f}".format(calcPerim()) , "ft around")
