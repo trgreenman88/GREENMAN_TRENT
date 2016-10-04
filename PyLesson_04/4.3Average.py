1c487de960b84333d93b114566eadeb0ca86f756
trgreenman88 <trgreenman88@gmail.com>
2016-10-04 10:21:49 -0700

    Lesson 4

--- PyLesson_04/4.3Average.py
+++ PyLesson_04/4.3Average.py
@@ -2,6 +2,7 @@ print("This program will help you to find the average of 3 numbers")
 num1 = float(input("What is your first number?"))
 num2 = float(input("What is your second number?"))
 num3 = float(input("What is your third number?"))
+
 def setNums(num1,num2,num3):
     global number1, number2, number3
     number1 = num1
@@ -12,3 +13,4 @@ def findAvg(number1, number2, number3):
     return (number1 + number2 + number3) / 3
 
 print("The average of" , num1, "," , num2, ", and" , num3, "is" , "{:00.5f}".format(findAvg(num1,num2,num3)) , ".")
+
