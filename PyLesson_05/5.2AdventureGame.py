c631a501d5ddf2bcac79439655fd05107013c6de
trgreenman88 <trgreenman88@gmail.com>
2016-10-20 10:25:24 -0700

    Worked on Lesson 5.2

---
+++ PyLesson_05/5.2AdventureGame.py
@@ -0,0 +1,49 @@
+print("You will be given 3 choices to go on your adventure and you will need to find the rigt order to succeed.")
+print("choice1 is Go through the woods")
+print("choice2 is Go to Grandma's house")
+print("choice3 is go over the river")
+def Adventure():
+    choice = input("What would you like to do first?")
+    if choice == "choice3" or "choice2" or "choice1":
+        if choice == "choice3":
+            print("Nicely done!")
+        else:
+            print("Not quite. Try another path")
+            Adventure()
+    else:
+        print("Please enter \"choice1\", \"choice2\", or \"choice3\"")
+        Adventrure()
+Adventure()
+
+
+def SecondStep():
+    choice = input("What would you like to do next?")
+    if choice == "choice3" or "choice2" or "choice1":
+        if choice == "choice1":
+            print("Geat job!")
+        else:
+            print("Not quite. Try another path")
+            SecondStep()
+    else:
+        print("Please enter \"choice1\", \"choice2\", or \"choice3\"")
+        SecondStep()
+SecondStep()
+
+def LastStep():
+    choice = input("What would you like to do to finish your journey?")
+    if choice == "choice3" or "choice2" or "choice1":
+        if choice == "choice2":
+            print("YAAAAAAAAAY!!! YOU SUCCESSCULLY COMPLETED YOUR JOURNEY!!! YOU WIN!!!")
+        else:
+            print("Not quite. Try another path")
+            LastStep()
+    else:
+        print("Please enter \"choice1\", \"choice2\", or \"choice3\"")
+        LastStep()
+LastStep()
+
+        
+    
+            
+            
+            
