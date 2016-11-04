34a785cc5bfb995795e9b9c78416c1b247a1e5c3
trgreenman88 <trgreenman88@gmail.com>
2016-11-04 10:25:19 -0700

    finished lesson 7

--- PyLesson_07/ReplaceA's.py
+++ PyLesson_07/ReplaceA's.py
@@ -1,5 +1,5 @@
 sentence = input("Please enter a sentence and make sure to include the letter \"a\": ")
 top = 0
 while top < sentence.count("a") > 0:
-    sentence = sentence[0 : sentence.index("a")] + sentence[sentence.index("a")+1 : len(sentence)]
+    sentence = sentence[0 : sentence.index("a")] + "@" + sentence[sentence.index("a")+1 : len(sentence)]
 print(sentence)
