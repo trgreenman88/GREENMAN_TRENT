8b28d14a4e7f57b144ff46d6d4875644c4a0b6ab
trgreenman88 <trgreenman88@gmail.com>
2016-11-28 10:24:22 -0800

    finished first letter

---
+++ PyLesson_09/AverageNumbers.py
@@ -0,0 +1,5 @@
+import random
+numbers = []
+for i in range(0, 10):
+    numbers[random.randint(0, 100)]
+print(numbers)
--- PyLesson_09/FirstLetter.py
+++ PyLesson_09/FirstLetter.py
@@ -1,7 +1,18 @@
 print("First letters...")
 words = ["Fries", "Burger", "Shake", "Onion Rings", "Soda"]
-output = ""
-for i in range(0, len(words)):
-    output += words[i] + " "
-print(output[0:len(words)])
+
+def first():
+    output = ""
+    global words
+    for word in words:
+        output += word[0] + " "
+    print(output)
+first()
+
+
+
+
+
+
+
 
