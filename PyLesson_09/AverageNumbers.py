b82cbc66181170dbce49584b5ded7fe9c2ee1f65
trgreenman88 <trgreenman88@gmail.com>
2016-12-01 10:25:26 -0800

    almost finished with 9

--- PyLesson_09/AverageNumbers.py
+++ PyLesson_09/AverageNumbers.py
@@ -1,7 +1,20 @@
+import random
 numbers = []
 for i in range(0, 10):
-    import random
-    numbers = str(random.randint(0, 100))
-print(str(numbers[i]))
+    numbers.append(random.randint(0, 100))
+print(numbers)
+
+output = ""
+for i in numbers:
+    output += str(i) + " "
+print(output)
+print("")
+def average(numbers):
+    avg = 0
+    for i in numbers:
+        avg += output
+        return avg / 10
+print("The average or the above numbers is... " + average(numbers))
+
 
 
--- PyLesson_09/FibonacciSequence.py
+++ PyLesson_09/FibonacciSequence.py
@@ -4,10 +4,10 @@ size = int(input("Please enter the length of your sequence: "))
 seq = []
 for i in range(0, size):
     if i == 0 or i == 1:
-        seq = start
+        seq.append(start)
     else:
-        seq = (i-1) + (i-2)
-print(seq[i] + " ")
+        seq.append(seq[i-1] + seq[i-2])
+print(seq)
         
     
     
