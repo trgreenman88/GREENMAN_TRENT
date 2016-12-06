a1870a9676d48295ba2859763dcd77cacc005cad
trgreenman88 <trgreenman88@gmail.com>
2016-12-06 10:23:08 -0800

    almost finished 9.1

--- PyLesson_09/9.1ExpressionSolver.py
+++ PyLesson_09/9.1ExpressionSolver.py
@@ -1,13 +1,23 @@
-expression = input("Please enter an expression without spaces: ")
-eqn = expression.split("")
+expression = input("Please enter an expression with spaces: ")
+eqn = expression.split(" ")
+i = 0
 while i < len(eqn):
-    if i < len(eqn) and eqn[i] == "*" or "/":
+    if i < len(eqn) and eqn[i] == "*" or eqn[i] == "/":
         if eqn[i] == "*":
             eqn[i] = int(eqn[i-1]) * int(eqn[i+1])
         else:
-            equation[i] = int(eqn[i-1]) / int(eqn[i+1])
+            eqn[i] = float(eqn[i-1]) / float(eqn[i+1])
         eqn.pop(i-1)
         eqn.pop(i)
     i += 1
 i = 0
-while i < len(eqn)
+while i < len(eqn):
+    if i < len(eqn) and eqn[i] == "+" or eqn[i] == "-":
+        if eqn[i] == "+":
+            eqn[i] = int(eqn[i-1]) + int(eqn[i+1])
+        else:
+            eqn[i] = int(eqn[i-1]) - int(eqn[i+1])
+        eqn.pop(i-1)
+        eqn.pop(i)
+    i += 1
+print(eqn)
--- PyLesson_09/9.1KeepComposites.py
+++ PyLesson_09/9.1KeepComposites.py
@@ -2,7 +2,7 @@ nums = [2,6,8,9,10,12,13,15,17,24,55,66,78,77,79]
 number = 0
 def gFactor(number):
     for i in range(0, number):
-        if nums[i]/2 == int(number):
+        if nums[i]/i == int(number):
             return 1
         else:
             return 0
