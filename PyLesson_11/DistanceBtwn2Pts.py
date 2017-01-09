08cf08aa5649f961686e3bacc764f153b84f142c
trgreenman88 <trgreenman88@gmail.com>
2017-01-09 10:17:13 -0800

    Finished exercise 1 for lesson 11

---
+++ PyLesson_11/DistanceBtwn2Pts.py
@@ -0,0 +1,8 @@
+import math
+class Distance:
+    def __init__(self, x1, x2, y1, y2):
+        self.xOne = x1
+        self.xTwo = x2
+        self.yOne = y1
+        self.yTwo = y2
+        
--- PyLesson_11/Exercise_01.py
+++ PyLesson_11/Exercise_01.py
@@ -4,35 +4,39 @@ class MilesPerHour:
         self.hrs = hours
         self.mins = minutes
     mph = 0
-    def modify(self, newdist, newhrs, newmins):
+    def setDist(self, newdist):
         self.dist = newdist
+    def setHrs(self, newhrs):
         self.hrs = newhrs
+    def setMins(self, newmins):
         self.mins = newmins
-    def getdistance():
+    def getdistance(self):
         return self.dist
-    def gethours():
+    def gethours(self):
         return self.hrs
-    def getminutes():
+    def getminutes(self):
         return self.mins
-    mph = dist/(hrs + (mins / 60))
+    def getMPH(self):
+        mph = self.dist/(self.hrs + (self.mins / 60))
+        return mph
 def main():
     dist = float(input("Please enter a distance in miles: "))
     hrs = int(input("Please enter a time in hours: "))
     mins = int(input("Plese enter a time in minutes: "))
-    mph1 = milesperhour(dist, hrs, mins)
+
+    mph1 = MilesPerHour(dist, hrs, mins)
 
     print("Distance: ", mph1.getdistance())
     print("Time: ", mph1.gethours(), "hours and ", mph1.getminutes()," minutes")
-    print("Speed: ", mph)
+    print("Speed: ", mph1.getMPH())
 
-    mph1.modify(newdist, newhrs, newmins)
-    newdist = 10
-    newhrs = 2
-    newmins = 0
+    mph1.setDist(10)
+    mph1.setHrs(2)
+    mph1.setMins(0)
 
     print("Distance: ", mph1.getdistance())
     print("Time: ", mph1.gethours(), "hours and ", mph1.getminutes()," minutes")
-    print("Speed: ", mph)
+    print("Speed: ", mph1.getMPH())
 main()
     
     
